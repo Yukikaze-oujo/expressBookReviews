da48c2e9709dbffe13e0d21f0528499badd2b2f4
Yukikaze <67070274@kmitl.ac.th>
2026-09-04 18:33:49 +0700

    add code for all question
    edited 1

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -28,3 +28,37 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({ data: password }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).send("Customer successfully logged in");
+  }
+  return res.status(208).json({ message: "Invalid Login. Check username and password" });
+});
+
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+    books[isbn].reviews[username] = review;
+    return res.status(200).send(`The review for the book with ISBN ${isbn} has been added/updated.`);
+  }
+  return res.status(404).json({ message: "Book not found" });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn] && books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res.status(200).send(`Reviews for the ISBN ${isbn} posted by the user ${username} deleted.`);
+  }
+  return res.status(404).json({ message: "Review not found" });
+});
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,3 +41,70 @@ public_users.get('/review/:isbn',function (req, res) {
 });
 
 module.exports.general = public_users;
+
+public_users.get('/', function (req, res) {
+  return res.status(200).send(JSON.stringify(books, null, 4));
+});
+
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  }
+  return res.status(404).json({ message: "Book not found" });
+});
+
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  let results = [];
+  for (let key in books) {
+    if (books[key].author === author) {
+      results.push(books[key]);
+    }
+  }
+  return res.status(200).json({ booksbyauthor: results });
+});
+
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  let results = [];
+  for (let key in books) {
+    if (books[key].title === title) {
+      results.push(books[key]);
+    }
+  }
+  return res.status(200).json({ booksbytitle: results });
+});
+
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn].reviews);
+  }
+  return res.status(404).json({ message: "Book not found" });
+});
+
+public_users.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Unable to register user." });
+  }
+  if (users.find(u => u.username === username)) {
+    return res.status(400).json({ message: "User already exists!" });
+  }
+  users.push({ username, password });
+  return res.status(200).json({ message: "Customer successfully registered. Now you can login" });
+});
+
+// ตัวอย่างการใช้ Axios เรียก Async/Promise ใน general.js
+const axios = require('axios');
+
+// Task 10: Get all books using async-await
+public_users.get('/async/books', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
