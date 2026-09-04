cceefd0a9297a0bd1b19f509049ef4ea185af6b0
Yukikaze <67070274@kmitl.ac.th>
2026-09-04 19:21:45 +0700

    edited code for q2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,49 +3,29 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "Customer successfully registered. Now you can login" });
+    } else {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(400).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-module.exports.general = public_users;
-
 public_users.get('/', function (req, res) {
   return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
+// Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   if (books[isbn]) {
@@ -54,6 +34,7 @@ public_users.get('/isbn/:isbn', function (req, res) {
   return res.status(404).json({ message: "Book not found" });
 });
 
+// Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
   let results = [];
@@ -65,6 +46,7 @@ public_users.get('/author/:author', function (req, res) {
   return res.status(200).json({ booksbyauthor: results });
 });
 
+// Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
   let results = [];
@@ -76,6 +58,7 @@ public_users.get('/title/:title', function (req, res) {
   return res.status(200).json({ booksbytitle: results });
 });
 
+// Get book review
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   if (books[isbn]) {
@@ -83,21 +66,6 @@ public_users.get('/review/:isbn', function (req, res) {
   }
   return res.status(404).json({ message: "Book not found" });
 });
-
-public_users.post("/register", (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).json({ message: "Unable to register user." });
-  }
-  if (users.find(u => u.username === username)) {
-    return res.status(400).json({ message: "User already exists!" });
-  }
-  users.push({ username, password });
-  return res.status(200).json({ message: "Customer successfully registered. Now you can login" });
-});
-
-//------------
-const axios = require('axios');
 
 // Task 10: Get all books using async-await
 public_users.get('/async/books', async function (req, res) {
@@ -111,34 +79,36 @@ public_users.get('/async/books', async function (req, res) {
 
 // Task 11: Get book details based on ISBN using Promises
 public_users.get('/async/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    axios.get(`http://localhost:5000/isbn/${isbn}`)
-        .then(response => {
-            return res.status(200).json(response.data);
-        })
-        .catch(error => {
-            return res.status(500).json({ message: error.message });
-        });
+  const isbn = req.params.isbn;
+  axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => {
+      return res.status(200).json(response.data);
+    })
+    .catch(error => {
+      return res.status(500).json({ message: error.message });
+    });
 });
 
 // Task 12: Get book details based on Author using async-await
 public_users.get('/async/author/:author', async function (req, res) {
-    try {
-        const author = req.params.author;
-        const response = await axios.get(`http://localhost:5000/author/${author}`);
-        return res.status(200).json(response.data);
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
+  try {
+    const author = req.params.author;
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 // Task 13: Get book details based on Title using async-await
 public_users.get('/async/title/:title', async function (req, res) {
-    try {
-        const title = req.params.title;
-        const response = await axios.get(`http://localhost:5000/title/${title}`);
-        return res.status(200).json(response.data);
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
+  try {
+    const title = req.params.title;
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
+
+module.exports.general = public_users;
